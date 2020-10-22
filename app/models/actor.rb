2a2c532987fcaf3d4374fa1f92f6712ac260b0eb
Cloudymolecule <baires82@gmail.com>
2020-10-22 18:44:02 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+    
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    f_name = self.first_name
+    l_name = self.last_name
+    name = "#{f_name} #{l_name}"
+    name
+  end
+
+  def list_roles
+    self.characters.map do |c|
+        roles = "#{c.name} - #{c.show.name}" 
+    end
+    #c.name
+    #c.show.name
+    
+    
+  end
+
 end
